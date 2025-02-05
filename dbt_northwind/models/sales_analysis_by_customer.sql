ee3fb4a1a07051ee047a7278c570a9fc762b3054
ben-bowman <benjamintbowman@gmail.com>
2025-02-05 12:14:10 -0500

    fix sales analysis model

--- dbt_northwind/models/sales_analysis_by_customer.sql
+++ dbt_northwind/models/sales_analysis_by_customer.sql
@@ -4,8 +4,8 @@ with customer_orders as (
         c.companyname,
         sum(od.quantity * od.unitprice) as gross_sales,
         avg(od.quantity * od.unitprice) as avg_gross_order_value,
-        sum((od.UnitPrice * od.Quantity * (1-od.Discount)) AS net_sales,
-        avg((od.UnitPrice * od.Quantity * (1-od.Discount)) as avg_net_order_value,
+        sum(od.UnitPrice * od.Quantity * (1-od.Discount)) AS net_sales,
+        avg(od.UnitPrice * od.Quantity * (1-od.Discount)) as avg_net_order_value,
         count(distinct o.orderid) as total_orders
     from {{ source('northwind', 'customers') }} c
     join {{ source('northwind', 'orders') }} o on c.customerid = o.customerid
